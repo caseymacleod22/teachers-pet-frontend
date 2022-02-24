b4dd9b487b187ca75227f960dc598b7849d9ed1b
Casey MacLeod <caseymacleod22@gmail.com>
2022-02-23 22:59:27 -0500

    testing frontend setup

---
+++ public/javascript/script.js
@@ -0,0 +1,20 @@
+let largeMemes;
+
+function bigTest () {
+    if(largeMemes === true) {
+        console.log('yea, this guys gaming')
+    } else {
+        console.log('dang, he really do be grinding lol')
+    }
+}
+
+function toggleMemes () {
+    if(largeMemes === true) {
+        console.log('here be your memes')
+    } else {
+        console.log('no memes today b?')
+    }
+}
+
+bigTest()
+toggleMemes()
